787b4337c6513896e1afc5e89d4a0334a8bfd90a
Wing.C4D <mikmuk1@gmail.com>
2025-04-26 22:56:57 +0300

    almost done2

--- src/main.py
+++ src/main.py
@@ -1,49 +1,56 @@
-from os import path, listdir,mkdir
+from os import path, listdir, mkdir
 from shutil import copy, rmtree
 from markdown_to_html_node import *
-
+from sys import argv
 
 def main():
-    dir_to_files('static', 'public')
-    generate_page('content/index.md', 'template.html', 'public/index.html')
-    dir_to_files('content', 'public')
-    generate_pages_recursive('content', 'template.html', 'public')
+    basepath = argv[0]
+    print(basepath)
+    if not basepath:
+        basepath = '/'
+    
+    generate_pages_recursive('static','template.html' , 'docs', basepath)
+    generate_pages_recursive('content', 'template.html', 'docs', basepath)
+    
+    
+def generate_page(from_path: str, template_path: str, dst_path: str, basepath: str) -> None:
+    print(f'Generating page from {from_path} to {dst_path} using {template_path}')
+    with open(from_path, 'r') as rf, open(template_path, 'r') as tmp_f, open(dst_path,'w') as wf:
+        basepath = '{'+basepath+'}'
+        md = rf.read()
+        html_str = markdown_to_html_node(md).to_html()
+        title = extract_title(md)
+        tmp_contents = tmp_f.read(
+            ).replace(r'{{ Title }}', title
+                      ).replace(r'{{ Content }}',html_str
+                                ).replace('href="/', f'href="{basepath}'
+                                          ).replace('src="/', f'src="{basepath}')
+        wf.write(str(tmp_contents))
 
-
-def generate_pages_recursive(dir_path_content: str, template_plath: str, dest_dir_path: str) -> None:
+def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str, basepath: str) -> None:
+    dir_to_files(dir_path_content, dest_dir_path)
     content_items_lst = listdir(dir_path_content)
+    print(f'parsing: {dir_path_content} to: {dest_dir_path} using: {template_path}')
     for item in content_items_lst:
         candidate_path = path.join(dir_path_content, item)
         dest_candidate_path = path.join(dest_dir_path, item)
         if path.isfile(candidate_path) and candidate_path.split('.')[1] == 'md':
-            dest_candidate_path = path.join(dest_dir_path, f'{item.rstrip('md')}html')
-            with open(candidate_path, 'r') as rf, open(template_plath, 'r') as tmp_f, open(dest_candidate_path, 'w') as wf:
-                md = rf.read()
-                html_str = markdown_to_html_node(md).to_html()
-
-                title = extract_title(md)
-                wf.write(tmp_f.read().replace(r'{{ Content }}', html_str).replace(r'{{ Title }}', title))
+            generate_page(candidate_path, template_path, f'{dest_candidate_path.rstrip('md')}html', basepath)
         elif path.isdir(candidate_path):
-            generate_pages_recursive(candidate_path, template_plath, dest_candidate_path)
-        continue
+            generate_pages_recursive(candidate_path, template_path, dest_candidate_path, basepath)
                 
                                 
 
-def dir_to_files(dir_path: str, dst_path: str) -> list[str]:
-    if not path.isfile(dst_path) and not path.isdir(dir_path):
-        for i, dir in enumerate(dst_path.split('/')):
-            if not path.isdir(dir) and i > len(dst_path.split('/')-1):
-                print(path.isdir('/'.join(dst_path.split('/')[:i])))
-            
+def dir_to_files(dir_path: str, dst_path: str) -> None:
     dir_items = listdir(dir_path)
     for item in dir_items:
-        if path.isfile(path.join(dir_path, item)):
-            copy(path.join(dir_path, item), dst_path)
-        elif path.isdir(path.join(dir_path, item)):
-            mkdir(path.join(dst_path, item))
-            print(path.isdir(path.join(dst_path, item)))
-            print(dir_to_files(path.join(dir_path, item), path.join(dst_path, item)))
-    return listdir(dst_path)
+        source_path = path.join(dir_path, item)
+        end_path = path.join(dst_path, item)
+        if path.isfile(source_path):
+            copy(source_path, end_path)
+        elif path.isdir(source_path) and not path.isdir(end_path):
+            mkdir(end_path)
+            dir_to_files(source_path, end_path)
 
 
 if __name__ == "__main__":
